1c119d49c202278304522dfe66c7f49e05e4531a
Simreich Somogod <simreich1989@gmail.com>
2025-08-12 17:40:11 +0800

    feature component done

--- src/components/TitleCom.vue
+++ src/components/TitleCom.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
 interface TitleProps {
-  titleSize?: string;
-  subtitleSize?: string;
+  titleClass?: string;
+  subtitleClass?: string;
 }
 
 const props = withDefaults(defineProps<TitleProps>(), {
-  titleSize: "text-2xl  max-w-[405px]",
-  subtitleSize: "text-[16px] max-w-[405px]",
+  titleClass: "text-2xl  max-w-[405px]",
+  subtitleClass: "text-[16px] max-w-[405px]",
 });
 </script>
 
@@ -20,17 +20,17 @@ const props = withDefaults(defineProps<TitleProps>(), {
     </article>
 
     <!-- Title & Subheading -->
-    <div v-if="$slots.subtitle" class="titles">
-      <h2 :class="['title-wrapper', props.titleSize]">
+    <div v-if="$slots.subtitle" class="title-wrapper">
+      <h2 :class="['title', props.titleClass]">
         <slot name="title">Default Title</slot>
       </h2>
-      <p :class="['subtitle-wrapper', props.subtitleSize]">
+      <p :class="['subtitle', props.subtitleClass]">
         <slot name="subtitle">Default Subheading</slot>
       </p>
     </div>
 
     <!-- Only Title -->
-    <h1 v-else :class="['title-wrapper', titleSize]">
+    <h1 v-else :class="['title-wrapper', titleClass]">
       <slot name="title">Default Title</slot>
     </h1>
   </div>
@@ -40,7 +40,7 @@ const props = withDefaults(defineProps<TitleProps>(), {
 @reference 'tailwindcss';
 
 .title-container {
-  @apply flex flex-col max-w-[336px] md:max-w-[600px] lg:max-w-[650px]  mx-auto gap-8 justify-center items-start md:items-center;
+  @apply flex flex-col md:max-w-[600px] lg:max-w-[650px]  mx-auto gap-8 justify-center items-start md:items-center;
 }
 
 .pill-wrapper {
@@ -52,16 +52,16 @@ const props = withDefaults(defineProps<TitleProps>(), {
   color: var(--color-washedPurple-400);
 }
 
-.titles {
+.title-wrapper {
   @apply flex flex-col gap-y-2;
 }
 
-.title-wrapper {
+.title {
   @apply text-left font-bold md:text-center;
   color: var(--color-washedPurple-400);
 }
 
-.subtitle-wrapper {
+.subtitle {
   @apply md:text-center;
   color: var(--color-washedPurple-700);
 }
--- src/sections/FeaturesSection.vue
+++ src/sections/FeaturesSection.vue
@@ -3,19 +3,45 @@ import TitleCom from "@/components/TitleCom.vue";
 </script>
 
 <template>
-  <section class="my-15 px-[25px] md:px-[45px]">
-    <TitleCom titleSize="text-[25px] md:text-[35px] lg:text-[45px]" subtitleSize="text-[16px] ">
-      <template #pill> Features </template>
+  <section class="my-10">
+    <div class="title-wrapper">
+      <TitleCom titleClass="text-[25px] md:text-[35px] lg:text-[45px]" subtitleClass="text-[16px] ">
+        <template #pill> Features </template>
 
-      <template #title> Keep track of your meetings all in one place</template>
+        <template #title> Keep track of your meetings all in one place</template>
 
-      <template #subtitle>
-        Capture your ideas, thoughts, and meeting notes in a structured and organized manner.
-      </template>
-    </TitleCom>
+        <template #subtitle>
+          Capture your ideas, thoughts, and meeting notes in a structured and organized manner.
+        </template>
+      </TitleCom>
+    </div>
+
+    <div class="app-calender">
+      <img src="../assets/images/app-calendar.png" alt="Calendar app" />
+      <div class="fade"></div>
+    </div>
   </section>
 </template>
 
 <style scoped>
 @reference 'tailwindcss';
+
+.title-wrapper {
+  @apply my-15 px-[25px] md:px-[45px];
+}
+
+.app-calender {
+  @apply relative flex justify-center items-center;
+}
+
+.app-calender img {
+  @apply relative -mt-4 ml-10 md:ml-0;
+}
+
+.app-calender .fade {
+  @apply absolute ml-15 -top-35 md:ml-0 w-[420px] h-[400.89px] md:w-[604px] md:h-[556.89px] md:-top-45  rounded-full -z-10;
+  background-color: var(--color-primaryPurple-700);
+  opacity: 0.5;
+  filter: blur(181.2px);
+}
 </style>
